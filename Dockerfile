bd9c2027f8f562b4cedaa90d94898157e264f7f8
Mike McQueen <mmcqueen@ruralking.com>
2019-12-10 14:32:01 -0600

    init commit

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+# FROM balenalib/raspberry3-pi-dotnet:3.0-sdk AS build
+# FROM mcr.microsoft.com/dotnet/core/sdk:3.0.100-preview6-alpine3.9 AS build
+FROM mcr.microsoft.com/dotnet/core/sdk:3.1-buster-arm32v7 AS build
+# FROM mcr.microsoft.com/dotnet/core/sdk:3.1-buster AS build
+
+WORKDIR /app
+
+# COPY ./qemu/qemu-arm /usr/bin/qemu-arm
+
+COPY ./beacon .
+
+RUN dotnet publish -r linux-arm -c Release -o ./deploy --self-contained true /p:PublishTrimmed=true
+
+FROM balenalib/raspberry-pi-alpine:3.8
+RUN install_packages \
+      bluez \
+      # Two dotnet dependencies (even for binary dontnet app)
+      libstdc++ libintl
+
+ENV UDEV=1
+ENV DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
+
+COPY --from=build /app/deploy ./app
+
+ENTRYPOINT [ "/bin/bash" ]
---
+++ beacon/Program.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RuralKing.Beacon
+{
+    class Program
+    {
+        static TimeSpan timeout = TimeSpan.FromSeconds(15);
+
+        static async Task Main(string[] args)
+        {
+            if (args.Length < 1 || args.Length > 2 || args[0].ToLowerInvariant() == "-h" || !int.TryParse(args[0], out int scanSeconds))
+            {
+                Console.WriteLine("Usage: scan <SecondsToScan> [adapterName]");
+                Console.WriteLine("Example: scan 15 hci0");
+                return;
+            }
+
+            IAdapter1 adapter;
+            if (args.Length > 1)
+            {
+                adapter = await BlueZManager.GetAdapterAsync(args[1]);
+            }
+            else
+            {
+                var adapters = await BlueZManager.GetAdaptersAsync();
+                if (adapters.Count == 0)
+                {
+                    throw new Exception("No Bluetooth adapters found.");
+                }
+
+                adapter = adapters.First();
+            }
+
+            var adapterPath = adapter.ObjectPath.ToString();
+            var adapterName = adapterPath.Substring(adapterPath.LastIndexOf("/") + 1);
+            Console.WriteLine($"Using Bluetooth adapter {adapterName}");
+
+            // Print out the devices we already know about.
+            var devices = await adapter.GetDevicesAsync();
+            foreach (var device in devices)
+            {
+                string deviceDescription = await GetDeviceDescriptionAsync(device);
+                Console.WriteLine(deviceDescription);
+            }
+            Console.WriteLine($"{devices.Count} device(s) found ahead of scan.");
+
+            Console.WriteLine();
+
+            // Scan for more devices.
+            Console.WriteLine($"Scanning for {scanSeconds} seconds...");
+
+            int newDevices = 0;
+            using (await adapter.WatchDevicesAddedAsync(async device => {
+                newDevices++;
+                // Write a message when we detect new devices during the scan.
+                string deviceDescription = await GetDeviceDescriptionAsync(device);
+                Console.WriteLine($"[NEW] {deviceDescription}");
+            }))
+            {
+                await adapter.StartDiscoveryAsync();
+                await Task.Delay(TimeSpan.FromSeconds(scanSeconds));
+                await adapter.StopDiscoveryAsync();
+            }
+            Console.WriteLine($"Scan complete. {newDevices} new device(s) found.");
+        }
+
+        private static async Task<string> GetDeviceDescriptionAsync(IDevice1 device)
+        {
+            var deviceProperties = await device.GetAllAsync();
+            return $"{deviceProperties.Alias} (Address: {deviceProperties.Address}, RSSI: {deviceProperties.RSSI})";
+        }
+    }
+}
